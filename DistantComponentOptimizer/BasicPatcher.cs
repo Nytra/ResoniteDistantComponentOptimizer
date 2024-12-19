7df721b97ae2ea22fad7bc2687118d8f753aee0e
Nytra <14206961+Nytra@users.noreply.github.com>
2024-12-19 18:07:24 +0000

    Wrap patch in try/catch and add SpreadUpdates option

--- DistantComponentOptimizer/BasicPatcher.cs
+++ DistantComponentOptimizer/BasicPatcher.cs
@@ -9,8 +9,12 @@ namespace DistantComponentOptimizer
 {
     internal class DistantComponentOptimizerConfig : ConfigSection
     {
-        public readonly DefiningConfigKey<int> UpdateInterval = new("UpdateInterval", "Interval between updates (roughly equiv. to frames)", () => 30, valueValidator: value => value > 0);
+        public readonly DefiningConfigKey<int> UpdateInterval = new("UpdateInterval", "Interval between updates (roughly equiv. to frames)", () => 30, valueValidator: value => value > 0 && value <= 60);
         public readonly DefiningConfigKey<float> ThrottleDistance = new("ThrottleDistance", "Distance user can be from slot before throttling updates (Meters)", () => 20f);
+        public readonly DefiningConfigKey<bool> SpreadUpdates = new("SpreadUpdates", "Spread out the updates more instead of running them all in one frame", () => false);
+        //public readonly DefiningConfigKey<Type> ExcludeType1 = new("ExcludeType1", "Excludes this type from throttling.");
+        //public readonly DefiningConfigKey<Type> ExcludeType2 = new("ExcludeType2", "Excludes this type from throttling.");
+        //public readonly DefiningConfigKey<Type> ExcludeType3 = new("ExcludeType3", "Excludes this type from throttling.");
 
         public override string Description => "Contains the config for DistantComponentOptimizer.";
         public override string Id => "DistantComponentOptimizer Config";
@@ -24,24 +28,50 @@ namespace DistantComponentOptimizer
         public override bool CanBeDisabled => true;
         private static bool Prefix(ComponentBase<Component> __instance)
         {
-            if (!Enabled || __instance.World.IsUserspace()) return true;
-            if (__instance.Enabled && __instance.CanRunUpdates && !__instance.UserspaceOnly)
+            try
             {
-                if (__instance.FindNearestParent<Slot>() is Slot slot && !slot.IsUnderLocalUser)
+                if (!Enabled || __instance.World.IsUserspace()) return true;
+                //if (ConfigSection.ExcludeType1 != null || ConfigSection.ExcludeType2 != null || ConfigSection.ExcludeType3 != null)
+                //{
+                //    var type = __instance.GetType();
+                //    if (ConfigSection.ExcludeType1 != null && type == ConfigSection.ExcludeType1) return true;
+                //    if (ConfigSection.ExcludeType2 != null && type == ConfigSection.ExcludeType2) return true;
+                //    if (ConfigSection.ExcludeType3 != null && type == ConfigSection.ExcludeType3) return true;
+                //}
+                if (__instance.Enabled && __instance.CanRunUpdates && !__instance.UserspaceOnly)
                 {
-                    var globPos = slot.GlobalPosition;
-                    var userPos = slot.World.LocalUserViewPosition;
-                    var num = ConfigSection.ThrottleDistance * __instance.LocalUserRoot?.GlobalScale ?? 1f;
-                    if (MathX.DistanceSqr(globPos, userPos) > num * num)
+                    if (__instance.FindNearestParent<Slot>() is Slot slot && !slot.IsUnderLocalUser)
                     {
-                        if (__instance.Time.LocalUpdateIndex % ConfigSection.UpdateInterval != 0) return false;
-
-                        // could use referenceID here to make the updates more spread out instead of running all in one frame
-                        //if ((__instance.Time.LocalUpdateIndex + (int)__instance.ReferenceID.Position) % ConfigSection.UpdateInterval != 0) return false;
+                        var globPos = slot.GlobalPosition;
+                        var userPos = slot.World.LocalUserViewPosition;
+                        var num = ConfigSection.ThrottleDistance * __instance.LocalUserRoot?.GlobalScale ?? 1f;
+                        if (MathX.DistanceSqr(globPos, userPos) > num * num)
+                        {
+                            if (ConfigSection.SpreadUpdates)
+                            {
+                                if ((__instance.Time.LocalUpdateIndex + (int)__instance.ReferenceID.Position) % ConfigSection.UpdateInterval != 0) return false;
+                            }
+                            else
+                            {
+                                if ((__instance.Time?.LocalUpdateIndex ?? 0) % ConfigSection.UpdateInterval != 0) return false;
+                            }
+                        }
                     }
                 }
+                return true;
             }
-            return true;
+            catch (Exception e)
+            {
+                try
+                {
+                    Logger.Error(() => e.ToString());
+                }
+                catch
+                {
+
+                }
+                return true;
+            }
         }
     }
 }
